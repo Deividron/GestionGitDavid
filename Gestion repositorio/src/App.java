eb17300ad44004a32895c95deff66a9e6e1b7564
DESKTOP-9PJA8HS\daw1 <droncerof01@educarex.es>
2023-09-26 19:25:35 +0200

    Fourth commit

--- Gestion repositorio/src/App.java
+++ Gestion repositorio/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Proyecto java");
         System.out.println("Foca saxofonista");
+        System.out.println("platano alcoholico");
+        System.out.println("Pepino cohete");
     }
 }
